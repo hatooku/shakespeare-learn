ce57ef80f8e072601d581ec23556341ca55a3897
hatooku <jhlee@caltech.edu>
2016-03-09 01:27:31 -0800

    HMM Algorithm Finished (Possible Debugging Needed)

--- hmm.py
+++ hmm.py
@@ -13,32 +13,40 @@ class HMM:
         self.PI = None # initial state distribution, 0-indexed
         self.O = None # observation (row: state; col: observation), 0-indexed
 
-    def train(self, data, scaling=True):
+    def train(self, data, epsilon=0.001, scaling=True):
         X = self.registerObs(data)
 
         # Initialize Matrices
-        """
         self.A = self.normalize(np.random.rand(L, L))
         self.PI = self.normalize(np.random.rand(L))
         self.O = self.normalize(np.random.rand(L, D))
-        """
-        # TESTING
-        self.A = np.array([[.6, .4], [.3, .7]])
-        self.PI = np.array([.8, .2])
-        self.O = np.array([[.3, .4, .3], [.4, .3, .3]])
 
-        alphas_arr = []
-        betas_arr = []
-        for seq in X:
-            alphas, betas = self.forwardBackward(seq, scaling)
-            print "alphas\n", alphas.transpose()
-            print "betas\n", betas.transpose()
-            alphas_arr.append(alphas)
-            betas_arr.append(betas)
-        #self.update(X, alphas_arr, betas_arr)
-        #print self.A
 
-        return
+        norm_arr = []
+        iterations = 0
+
+        while (True):
+            iterations += 1
+            # E Step
+            alphas_arr = []
+            betas_arr = []
+            for seq in X:
+                alphas, betas = self.forwardBackward(seq, scaling)
+                alphas_arr.append(alphas)
+                betas_arr.append(betas)
+
+            # M step (Computes marginals + Updates)
+            change_norm = self.update(X, alphas_arr, betas_arr)
+            norm_arr.append(change_norm)
+
+            # Stopping Condition
+            if len(norm_arr) > 1 and norm_arr[-1] / norm_arr[0] < epsilon:
+                print iterations
+                break
+
+        print h.PI
+        print h.A
+        print h.O
 
     """ Registers observations as integers and returns data transformed into
     integers. """
@@ -113,6 +121,52 @@ class HMM:
 
     def update(self, X, alphas_arr, betas_arr):
         L = self.L # num states
+        D = self.D # num unique tokens
+
+        # new matrices
+        PI = np.zeros(self.PI.shape)
+        A = np.zeros(self.A.shape)
+        O = np.zeros(self.O.shape)
+
+        # update O (emission matrix)
+        for state in range(L):
+            for token in range(D):
+                numerator = 0
+                denominator = 0
+                for j in range(len(X)): # iterate over all sequences
+                    seq = X[j]
+                    alphas = alphas_arr[j]
+                    betas = betas_arr[j]
+
+                    # for each index in seq
+                    for i in range(len(seq)):
+                        # compute P(y_i = z)
+                        top = alphas[i, state] * betas[i, state]
+                        bot = alphas[i].dot(betas[i])
+                        prob = top / bot
+
+                        if seq[i] == token: # indicator function
+                            numerator += prob
+                        denominator += prob
+                O[state, token] = numerator / denominator
+
+        # Make sure numbers add up to 1
+        O = self.normalize(O)
+
+        # update PI (initial distribution matrix)
+        for state in range(L):
+            prob_sum = 0
+            for j in range(len(X)): # iterate over all sequences
+                seq = X[j]
+                alphas = alphas_arr[j]
+                betas = betas_arr[j]
+
+                # compute P(y_0 = state)
+                prob_sum += alphas[0, state] * betas[0, state] / \
+                            alphas[0].dot(betas[0])
+            PI[state] = prob_sum / len(X)
+        # Make sure numbers add up to 1
+        PI = self.normalize(PI)
 
         # Update A (transition matrix)
         for prev in range(L):
@@ -124,7 +178,9 @@ class HMM:
                     seq = X[j]
                     alphas = alphas_arr[j]
                     betas = betas_arr[j]
-                    for i in range(len(seq)): # for each index in seq
+
+                    # for each index in seq excluding last index
+                    for i in range(len(seq)-1):
                         # Compute P(y_i = prev, y_i+1 = next) and add to
                         # numerator
                         # Names: numerator_top, numerator_bottom
@@ -134,7 +190,7 @@ class HMM:
                         # SHOULD BE PRECOMPUTED (FIX LATER)
                         for prev_state in range(L):
                             for next_state in range(L):
-                                bot1 += alphas[i, prev_state] * \
+                                num_bot += alphas[i, prev_state] * \
                                         self.O[next_state, seq[i+1]] * \
                                         self.A[prev_state, next_state] * \
                                         betas[i+1, next_state]
@@ -143,20 +199,40 @@ class HMM:
                         # Compute P(y_i = b) and add to denominator
                         # Names: denominator_top, denominator_bottom
                         denom_top = alphas[i, prev] * betas[i, prev]
-                        denom_bot = 0
-                        # SHOULD BE PRECOMPUTED (FIX LATER) OR DOT PRODUCT
-                        for state in len(L):
-                            denom_bot += alphas[i, state] * betas[i, state]
+                        denom_bot = alphas[i].dot(betas[i])
+
                         denominator += denom_top / denom_bot
 
                 # UPDATE A_{prev, next}
-                self.A[prev, next] = numerator / denominator
+                A[prev, next] = numerator / denominator
+
+        # Make sure numbers add up to 1
+        A = self.normalize(A)
+
+        # Calculate norm of change
+        # Frobenius norm of the differences between update and previous matrices
+        change_norm = np.linalg.norm(self.A - A) + np.linalg.norm(self.O - O) \
+                      + np.linalg.norm(self.PI - PI)
+
+        # update matrices
+        self.O = O
+        self.PI = PI
+        self.A = A
+
+        return change_norm
 
 # Testing
 testing = True
-
+# Rochester example
 if testing:
     h = HMM(2)
     print "\n"
     data = [['R', 'W', 'B', 'B']]
-    h.train(data, scaling=False)
+    h.train(data, scaling=True)
+"""
+# For testing, use these initial matrices (for Rochester example)
+self.A = np.array([[.6, .4], [.3, .7]])
+self.PI = np.array([.8, .2])
+self.O = np.array([[.3, .4, .3], [.4, .3, .3]])
+print "A:\n", self.A
+print "O:\n", self.O """
